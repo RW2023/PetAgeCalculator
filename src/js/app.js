d16f0fe2a6a0e15ab51692dce48bf0ddaa6ece52
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-04 03:05:29 -0500

    initial script no tests just added to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
 // Define variables  and functions I guess...?
-function calcDogAge(dogBirthYear, currentYear){
-let petAge = currentYear - dogBirthYear
+function calculateDogAge() {
+    let humanAge = document.getElementById("humanAge").value;
+    let breed = document.getElementById("breed").value;
+    let dogAge = null;
 
-}
+    if (breed === "small") {
+      dogAge = humanAge * 5;
+    } else if (breed === "medium") {
+      dogAge = humanAge * 6;
+    } else if (breed === "large") {
+      dogAge = humanAge * 7;
+    }
+
+    document.getElementById("result").textContent = `Your ${breed} dog's age in dog years is: ${dogAge}`;
+  }
+
