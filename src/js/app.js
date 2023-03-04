e9d8846569aff6625d9e8ab46e022a30214d49d2
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-04 03:18:06 -0500

    test script for dog age calculation. Works

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,20 @@
 // Define variables  and functions I guess...?
 function calculateDogAge() {
-    let humanAge = document.getElementById("humanAge").value;
-    let breed = document.getElementById("breed").value;
-    let dogAge = null;
+  let humanAge = document.getElementById("humanAge").value;
+  let breed = document.getElementById("breed").value;
+  let dogAge = null;
 
-    if (breed === "small") {
-      dogAge = humanAge * 5;
-    } else if (breed === "medium") {
-      dogAge = humanAge * 6;
-    } else if (breed === "large") {
-      dogAge = humanAge * 7;
-    }
-
-    document.getElementById("result").textContent = `Your ${breed} dog's age in dog years is: ${dogAge}`;
+  if (breed === "small") {
+    dogAge = humanAge * 5;
+  } else if (breed === "medium") {
+    dogAge = humanAge * 6;
+  } else if (breed === "large") {
+    dogAge = humanAge * 7;
   }
+
+  document.querySelector(
+    ".results"
+  ).textContent = `Your ${breed} dog's age in dog years is: ${dogAge}`;
+}
+
 
