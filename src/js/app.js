ef3aff86abfd8c8042fcf9442dd53b1c35119785
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-04 21:56:04 -0500

    Write function to toggle hidden div

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 // Define variables  and functions I guess...?
+// Function to calculate dog age 
 function calculateDogAge() {
   let humanAge = document.getElementById("humanAge").value;
   let breed = document.getElementById("breed").value;
@@ -17,4 +18,12 @@ function calculateDogAge() {
   ).textContent = `Your ${breed} dog's age in dog years is: ${dogAge}`;
 }
 
-
+// function to toggle hidden div
+function toggleHiddenDiv() {
+  var div = document.querySelector(".hiddenDiv");
+  if (div.style.display === "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
