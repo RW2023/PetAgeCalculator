723ea4bd8ba4fdc6ac56232aaf13c1322efedb9e
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-16 09:10:27 -0400

    add summary of switchstylsheet function

--- src/js/toggleStyles.js
+++ src/js/toggleStyles.js
@@ -4,3 +4,10 @@ function switchStylesheet() {
     const themeStylesheet = document.getElementById("theme-stylesheet");
     themeStylesheet.setAttribute("href", "./src/css/" + selectedTheme + ".css");
   }
+
+//   What this code does 
+
+// 1. it gets the DOM element with the ID "theme-select" using the document.getElementById method and stores it in a constant variable named "themeSelect".
+// 2. It gets the selected option value from the "themeSelect" dropdown list using the selectedIndex property and stores it in a constant variable named "selectedTheme".
+// 3. It gets the DOM element with the ID "theme-stylesheet" using the document.getElementById method and stores it in a constant variable named "themeStylesheet".
+// 4. It sets the href attribute of the "themeStylesheet" element to the path of the selected theme CSS file using the setAttribute method
